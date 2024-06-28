781995853acbb549833b5fa449cadd694b4f577d
JerichoAmargo <113118498+JerichoAmargo@users.noreply.github.com>
2024-06-28 20:44:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,30 @@ document.addEventListener("DOMContentLoaded", function() {
 
     type();
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const navbar = document.getElementById("navbarNav");
+    let lastScrollTop = 0;
+    const delta = 5;
+    const navbarHeight = navbar.offsetHeight;
+
+    window.addEventListener("scroll", function() {
+        let st = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (Math.abs(lastScrollTop - st) <= delta)
+            return;
+
+        if (st > lastScrollTop && st > navbarHeight){
+            // Scroll Down
+            navbar.style.top = `-${navbarHeight}px`;
+        } else {
+            // Scroll Up
+            if(st + window.innerHeight < document.documentElement.offsetHeight) {
+                navbar.style.top = "0px";
+            }
+        }
+
+        lastScrollTop = st;
+    }, false);
+});
+
